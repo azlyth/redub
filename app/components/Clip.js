2e7336297e056b511b36af4c0617590af97b2d28
Peter Valdez <peter@nycmesh.net>
2017-11-01 22:58:45 -0400

    Put clip styling in its own CSS file

--- app/components/Clip.js
+++ app/components/Clip.js
@@ -2,18 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import * as u from '../utils';
-
-
-const styles = {
-  dialogue: {
-    fontSize: '2rem',
-    marginBottom: '10px'
-  },
-
-  button: {
-    fontSize: '1.3rem'
-  },
-};
+import styles from './Clip.css';
 
 
 export default class Clip extends Component {
@@ -78,15 +67,27 @@ export default class Clip extends Component {
     this.playVideo(false);
   }
 
+  renderDialogue() {
+    // Remove any size attributes from the subtitle HTML
+    const dialogue = this.props.text.replace(/size="?\d*"?/g, '');
+
+    return (
+      <div
+        className={styles.dialogue}
+        dangerouslySetInnerHTML={{ __html: dialogue }}
+      />
+    );
+  }
+
   renderPlayDubButton() {
     if (this.state.recorded) {
       return (
         <span>
-          <Button style={styles.button} bsSize="small" bsStyle="success" onClick={this.handlePlayDub}>
+          <Button className={styles.button} bsSize="small" bsStyle="success" onClick={this.handlePlayDub}>
             <b>PLAY DUB</b>
           </Button>
           &nbsp;&nbsp;
-          <Button style={styles.button} bsSize="small" bsStyle="warning" onClick={this.handleRemoveDub}>
+          <Button className={styles.button} bsSize="small" bsStyle="warning" onClick={this.handleRemoveDub}>
             <b>REMOVE DUB</b>
           </Button>
           &nbsp;&nbsp;
@@ -117,21 +118,17 @@ export default class Clip extends Component {
       <div>
         <div>
 
-          <b>
-            <div
-              style={styles.dialogue}
-              dangerouslySetInnerHTML={{ __html: this.props.text }}
-            />
-          </b>
 
-          <Button style={styles.button} bsSize="small" bsStyle="success" onClick={this.handlePlay}>
+        { this.renderDialogue() }
+
+          <Button className={styles.button} bsSize="small" bsStyle="success" onClick={this.handlePlay}>
             <b>PLAY</b>
           </Button>
           &nbsp;&nbsp;
 
           { this.renderPlayDubButton() }
 
-          <Button style={styles.button} bsSize="small" bsStyle="danger" onClick={this.handleRecord}>
+          <Button className={styles.button} bsSize="small" bsStyle="danger" onClick={this.handleRecord}>
             <b>RECORD</b>
           </Button>
 
