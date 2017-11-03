273896d3786c3d68564fee5f1b4fcf5a0e2daba9
Peter Valdez <peter@nycmesh.net>
2017-11-03 18:29:24 -0400

    Remove the Counter page

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
 
 export default () => (
   <App>
